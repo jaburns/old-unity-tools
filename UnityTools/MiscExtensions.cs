f3be2419d62347fce3bc23af37d8b7d5c4a2af6d
Jeremy Burns <jeremyaburns@gmail.com>
2013-11-12 22:03:09 -0700

    Added Intersect extension to Rect

---
+++ UnityTools/MiscExtensions.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using UnityEngine;
+
+namespace UnityTools
+{
+    public static class MiscExtensions
+    {
+        static bool Intersect (this Rect rectA, Rect rectB) {
+            return Mathf.Abs(rectA.x - rectB.x) < Mathf.Abs(rectA.width  + rectB.width)  / 2
+                && Mathf.Abs(rectA.y - rectB.y) < Mathf.Abs(rectA.height + rectB.height) / 2;
+        }
+    }
+}
